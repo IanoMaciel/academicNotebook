8e5520fea8675eda9b7304457a2656a052be2491
Iano Maciel <ianomaciel6385@gmail.com>
2021-10-08 21:32:03 -0400

    Parte5: em andamento

---
+++ 2021.2/Programação Web/Exercícios_práticos/Lista1_parte5/js/app.js
@@ -0,0 +1,29 @@
+class Despesa
+{
+    constructor(ano, mes, dia, tipo, descricao, valor)
+    {
+        this.ano = ano
+        this.mes = mes
+        this.dia = dia
+        this.tipo = tipo
+        this.descricao = descricao
+        this.valor = valor
+    }
+}
+
+function cadastrarDespesa()
+{
+    // slecionando os campos 
+    let dia = document.getElementById('dia')
+    let mes = document.getElementById('mes')
+    let ano = document.getElementById('ano')
+    
+    let tipo = document.getElementById('tipo')
+    let descricao = document.getElementById('descricao')
+    let valor = document.getElementById('valor')
+
+    let despesa = new Despesa(dia.value, mes.value, ano.value, tipo.value, descricao.value, valor.value)
+
+    console.log(despesa)
+}
+
