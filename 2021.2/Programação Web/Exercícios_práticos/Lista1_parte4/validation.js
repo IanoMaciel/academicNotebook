546953972f5c62b35b4b012d7a7b248bd965566a
Iano Maciel <ianomaciel6385@gmail.com>
2021-10-25 20:05:42 -0400

    js: modificado e finalizado

--- 2021.2/Programação Web/Exercícios_práticos/Lista1_parte4/validation.js
+++ 2021.2/Programação Web/Exercícios_práticos/Lista1_parte4/validation.js
@@ -1,13 +1,17 @@
 function validation()
 {
     let cpf = document.getElementById('cpf').value;
-    let password = document.getElementById('senha').value
-
-    if(cpf == '' || cpf.length < 11 || password == '' || password.length < 6)
+    let senha = document.getElementById('senha').value;
+    let str = '';
+    if(cpf.length != 11 || isNaN(cpf))
     {
-        alert('CPF e/ou senha inválida')
+        str = 'CPF inválido! Digite 11 digitos com caracteres númericos. \nCPF: ' + cpf + '\nDigitos: ' + cpf.length; 
+        alert(str);
+    } else if(senha.length < 6 || isNaN(senha)) {
+        str = 'Sua senha deve conter pelo menos 6 digitos e todos com caracteres númericos';
+        alert(str);
     } else {
-        alert('Validação realizada com sucesso!')
+        alert('Validado com sucesso!');
+        
     }
-    
 }
